9ab4b161d0c480d782d1da7b139dc4552e21f70a
mkput <m.khawarilputra24@gmail.com>
2025-10-25 02:58:29 +0700

    fix: app consumer

--- src/app/consumer.ts
+++ src/app/consumer.ts
@@ -1,6 +1,6 @@
 import { RabbitMQConnection } from "$pkg/pubsub"
-import { PUBSUB_TOPICS } from "$entities/PubSub"
-import { KnowledgeQueueDTO } from "$entities/Knowledge"
+// import { PUBSUB_TOPICS } from "$entities/PubSub"
+// import { KnowledgeQueueDTO } from "$entities/Knowledge"
 
 export async function startConsumerApp() {
     const commonChannel = new RabbitMQConnection()
@@ -8,21 +8,21 @@ export async function startConsumerApp() {
     await commonChannel.setPrefetchCount(1)
 
     // Consume Messages Here
-    await commonChannel.consume(PUBSUB_TOPICS.KNOWLEDGE_UPDATE, async (message) => {
-        const knowledgeQueueDTO = JSON.parse(message) as KnowledgeQueueDTO
-        console.log("KNOWLEDGE_UPDATE")
-        console.log(knowledgeQueueDTO)
-    })
+    // await commonChannel.consume(PUBSUB_TOPICS.KNOWLEDGE_UPDATE, async (message) => {
+    //     const knowledgeQueueDTO = JSON.parse(message) as KnowledgeQueueDTO
+    //     console.log("KNOWLEDGE_UPDATE")
+    //     console.log(knowledgeQueueDTO)
+    // })
 
-    await commonChannel.consume(PUBSUB_TOPICS.KNOWLEDGE_CREATE, async (message) => {
-        const knowledgeQueueDTO = JSON.parse(message) as KnowledgeQueueDTO
-        console.log("KNOWLEDGE_CREATE")
-        console.log(knowledgeQueueDTO)
-    })
-    await commonChannel.consume(PUBSUB_TOPICS.KNOWLEDGE_DELETE, async (message) => {
-        const knowledgeQueueDTO = JSON.parse(message) as KnowledgeQueueDTO
-        console.log("KNOWLEDGE_DELETE")
-        console.log(knowledgeQueueDTO)
-    })
+    // await commonChannel.consume(PUBSUB_TOPICS.KNOWLEDGE_CREATE, async (message) => {
+    //     const knowledgeQueueDTO = JSON.parse(message) as KnowledgeQueueDTO
+    //     console.log("KNOWLEDGE_CREATE")
+    //     console.log(knowledgeQueueDTO)
+    // })
+    // await commonChannel.consume(PUBSUB_TOPICS.KNOWLEDGE_DELETE, async (message) => {
+    //     const knowledgeQueueDTO = JSON.parse(message) as KnowledgeQueueDTO
+    //     console.log("KNOWLEDGE_DELETE")
+    //     console.log(knowledgeQueueDTO)
+    // })
     // For each use case , will need different channel to consume event separately
 }
