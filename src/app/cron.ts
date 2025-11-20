517dfbffd9b3e03f3bb841d0cffc9b4192eaf837
mkput <m.khawarilputra24@gmail.com>
2025-11-21 02:54:10 +0700

    fix: cron schedule

--- src/app/cron.ts
+++ src/app/cron.ts
@@ -4,11 +4,7 @@ import * as AssignmentAttemptController from "$controllers/cron/AssignmentAttemp
 export async function startCronApp() {
     const cronServer = server.cronServer
 
-    cronServer.addController("Some Scheduled Task", "*/30 * * * * *", () => {
-        console.log("Some Scheduled Task")
-    })
-
-    cronServer.addController("Check Assignment Expired Date", "*/1 * * * * *", () => {
+    cronServer.addController("Check Assignment Expired Date", "*/1 * * * *", () => {
         AssignmentAttemptController.checkAssignmentExpiredDate()
     })
 
