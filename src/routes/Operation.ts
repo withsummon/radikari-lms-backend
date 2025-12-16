cdc89e92f7b4a3661d428e1667347c1b4ad03d23
mkput <m.khawarilputra24@gmail.com>
2025-12-16 21:02:28 +0700

    fix: operation route

--- src/routes/Operation.ts
+++ src/routes/Operation.ts
@@ -1,46 +1,28 @@
 import { Hono } from "hono"
 import * as OperationController from "$controllers/rest/OperationController"
 import * as AuthMiddleware from "$middlewares/authMiddleware"
-import { Roles } from "../../generated/prisma/client"
 import * as Validations from "$validations/OperationValidation"
 
 const OperationRoutes = new Hono()
 
-OperationRoutes.get(
-	"/",
-	AuthMiddleware.checkJwt,
-	AuthMiddleware.checkRole([Roles.ADMIN]),
-	OperationController.getAll,
-)
+OperationRoutes.get("/", AuthMiddleware.checkJwt, OperationController.getAll)
 
-OperationRoutes.get(
-	"/:id",
-	AuthMiddleware.checkJwt,
-	AuthMiddleware.checkRole([Roles.ADMIN]),
-	OperationController.getById,
-)
+OperationRoutes.get("/:id", AuthMiddleware.checkJwt, OperationController.getById)
 
 OperationRoutes.post(
-	"/",
-	AuthMiddleware.checkJwt,
-	AuthMiddleware.checkRole([Roles.ADMIN]),
-	Validations.validateOperationSchema,
-	OperationController.create,
+    "/",
+    AuthMiddleware.checkJwt,
+    Validations.validateOperationSchema,
+    OperationController.create
 )
 
 OperationRoutes.put(
-	"/:id",
-	AuthMiddleware.checkJwt,
-	AuthMiddleware.checkRole([Roles.ADMIN]),
-	Validations.validateOperationSchema,
-	OperationController.update,
+    "/:id",
+    AuthMiddleware.checkJwt,
+    Validations.validateOperationSchema,
+    OperationController.update
 )
 
-OperationRoutes.delete(
-	"/:id",
-	AuthMiddleware.checkJwt,
-	AuthMiddleware.checkRole([Roles.ADMIN]),
-	OperationController.deleteById,
-)
+OperationRoutes.delete("/:id", AuthMiddleware.checkJwt, OperationController.deleteById)
 
 export default OperationRoutes
