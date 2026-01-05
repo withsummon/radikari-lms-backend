346280280e8cdf86ec73555c859868342d48f7af
muhammadrayaarrizki <muhammadrayaarrizki@gmail.com>
2026-01-05 21:42:05 +0700

    fix: remove operations

--- src/repositories/OperationRepository.ts
+++ src/repositories/OperationRepository.ts
@@ -1,78 +1,30 @@
-import * as EzFilter from "@nodewave/prisma-ezfilter"
-import { prisma } from "$pkg/prisma"
-import { OperationDTO } from "$entities/Operation"
+import { prisma } from "$pkg/prisma";
+import { OperationDTO } from "$entities/Operation";
+import { ulid } from "ulid";
 
 export async function create(data: OperationDTO) {
 	return await prisma.operation.create({
-		data,
-	})
+		data: {
+			id: data.id || ulid(),
+			name: data.name,
+			description: data.description,
+			headOfOperationUserId: data.headOfOperationUserId,
+		},
+	});
 }
+
 export async function findByName(name: string) {
 	return await prisma.operation.findFirst({
-		where: {
-			name,
-		},
-	})
+		where: { name },
+	});
 }
 
-export async function getAll(filters: EzFilter.FilteringQuery) {
-	const queryBuilder = new EzFilter.BuildQueryFilter()
-	const { filters: rawFilters, ...rest } = filters
-	const usedFilters = queryBuilder.build(rest as any)
-
-	const [operation, totalData] = await Promise.all([
-		prisma.operation.findMany(usedFilters.query as any),
-		prisma.operation.count({
-			where: usedFilters.query.where,
-		}),
-	])
-
-	let totalPage = 1
-	if (totalData > usedFilters.query.take)
-		totalPage = Math.ceil(totalData / usedFilters.query.take)
-
-	return {
-		entries: operation,
-		totalData,
-		totalPage,
-	}
+export async function findFirst() {
+	return await prisma.operation.findFirst();
 }
 
 export async function getById(id: string) {
 	return await prisma.operation.findUnique({
-		where: {
-			id,
-		},
-		include: {
-			headOfOperationUser: {
-				select: {
-					id: true,
-					fullName: true,
-					email: true,
-				},
-			},
-			tenant: true,
-		},
-	})
-}
-
-export async function update(id: string, data: OperationDTO) {
-	return await prisma.operation.update({
-		where: {
-			id,
-		},
-		data,
-	})
-}
-
-export async function deleteById(id: string) {
-	return await prisma.operation.delete({
-		where: {
-			id,
-		},
-	})
-}
-
-export async function findFirst() {
-	return await prisma.operation.findFirst()
+		where: { id },
+	});
 }
